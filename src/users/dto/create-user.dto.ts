1638b5d888c36adacbb1b53c495c8867f6cd69e9
ladiesman217 <septillion433@gmail.com>
2025-12-05 22:04:23 +0300

    validate register

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,7 +1,22 @@
+import { IsEmail, IsEnum, IsOptional, IsString, MinLength } from "class-validator";
+
 export class CreateUserDto {
+    @IsString()
+    @MinLength(3, { message: 'Никнейм слишком короткий' })
     nickname: string;
+
+    @IsEmail({}, { message: 'Некорректный Email' })
     email: string;
+
+    @IsString()
+    @MinLength(6, { message: 'Пароль должен быть минимум 6 символов' })
     password: string;
+
+    @IsOptional()
+    @IsString()
     bio?: string;
+
+    @IsOptional()
+    @IsEnum(['USER', 'MANAGER', 'ADMIN'])
     role?: 'USER' | 'MANAGER' | 'ADMIN';
 }
